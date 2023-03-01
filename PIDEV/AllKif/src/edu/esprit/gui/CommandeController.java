c33270a8693bc7b618d4f605de9359c2ba86b70d
zayatisarah <80324188+zayatisarah@users.noreply.github.com>
2023-03-02 00:15:18 +0100

    version5

--- PIDEV/AllKif/src/edu/esprit/gui/CommandeController.java
+++ PIDEV/AllKif/src/edu/esprit/gui/CommandeController.java
@@ -9,18 +9,26 @@ import edu.esprit.entites.commande;
 import edu.esprit.entites.panier;
 import edu.esprit.services.ServiceCommande;
 import edu.esprit.services.ServicePanier;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -30,58 +38,65 @@ import javafx.scene.input.MouseEvent;
 public class CommandeController implements Initializable {
 
     @FXML
-    private TableView<commande> table;
-    @FXML
-    private TableColumn<commande, String> img;
-    @FXML
-    private TableColumn<commande, String> nom;
-    @FXML
-    private TableColumn<commande, String> desc;
-    @FXML
-    private TableColumn<commande, Integer> price;
-    @FXML
-    private TableColumn<commande, Integer> quan;
-    @FXML
     private Button fx_cc;
     @FXML
     private Button fx_AC;
-    
-    ServicePanier ps = new ServicePanier(); 
-    ServiceCommande commande = new ServiceCommande(); 
-
+    @FXML
+    private TableView<commande> total;
+    @FXML
+    private TableColumn<commande, Integer> commande;
+    ServiceCommande pc = new ServiceCommande(); 
     /**
      * Initializes the controller class.
      */
-    @Override 
-    public void initialize(URL url, ResourceBundle rb) {  
-        
-        img.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPic()));
-        nom.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
-        price.setCellValueFactory(cellData -> new SimpleIntegerProperty((int) cellData.getValue().getPrice()).asObject());
-        //xrole.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRole()));
-        desc.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDescription()));
-       
-        //id_prod.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getId_prod()).asObject()); 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        commande.setCellValueFactory(cellData -> new SimpleIntegerProperty((int) cellData.getValue().getTotale()).asObject()); 
          ServiceCommande crud = new ServiceCommande();
-         ServicePanier khouloud = new ServicePanier();
         // Populate the table with data
         List<commande> data;
-        List<panier> panier;
-
-        data = crud.SelectAll();
-     
-        table.getItems().setAll(data);
-        
-        // TODO
+        data = crud.getAll();
+        total.getItems().setAll(data);
     }    
 
     @FXML
-    private void confirmer_commande(MouseEvent event) {
+    private void confirmer_commande(MouseEvent event) { 
+         try {
+
+Parent page2 =
+FXMLLoader.load(getClass().getResource("confc.fxml"));
+Scene scene = new Scene(page2);
+Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+stage.setScene(scene);
+stage.show();
+} catch (IOException ex) {
+
+
+}
+        
     }
 
     @FXML
-    private void annuler_commande(MouseEvent event) {
+    private void annuler_commande(MouseEvent event) { 
+         if (total.getSelectionModel().getSelectedItem() != null) {
+            // Récupérer les données de l'événement sélectionné
+            commande selectedCommande = total.getSelectionModel().getSelectedItem();
+            ServiceCommande sc = new ServiceCommande();
+            sc.supprimer(selectedCommande.getId_commande());
+        }
+        try {
+            Parent page1 = FXMLLoader.load(getClass().getResource("Commande.fxml"));
+            Scene scene = new Scene(page1);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ex) {
+            Logger.getLogger(CommandeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    
 
     @FXML
     private void facture(ActionEvent event) {
